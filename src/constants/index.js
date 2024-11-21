8505ecf13f93acb2d4b8b7df776aaf82b5f05efb
Priyansh khanna <145784273+priyansh-26@users.noreply.github.com>
2024-11-21 11:53:14 +0530

    new work

--- src/constants/index.js
+++ src/constants/index.js
@@ -46,12 +46,12 @@ const services = [
     icon: web,
   },
   {
-    title: "React Native Developer",
-    icon: mobile,
-  },
-  {
     title: "Backend Developer",
     icon: backend,
+  },
+  {
+    title: "Flutter Developer",
+    icon: mobile,
   },
   {
     title: "Content Creator",
